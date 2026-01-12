dd28225a21674c75c5b018aeb02332711c8e8dfb
Egor312-33 <186381521+Egor312-33@users.noreply.github.com>
2026-01-12 23:56:20 +0700

    feat: add rmq interface and validator

--- lib/config/interfaces/all-configs.interface.ts
+++ lib/config/interfaces/all-configs.interface.ts
@@ -1,12 +1,14 @@
 
 import type { DatabaseConfig } from './database.interface'
 import type { GrpcConfig } from './grpc.interface'
-import { JwtConfig } from './jwt.interface'
-import { RedisConfig } from './redis.interface'
+import type { JwtConfig } from './jwt.interface'
+import type { RedisConfig } from './redis.interface'
+import type { RmqConfig } from './rmq.interface'
 
 export interface AllConfigs {
     grpc: GrpcConfig
     database: DatabaseConfig
     redis: RedisConfig
-    jwt: JwtConfig
+    jwt: JwtConfig,
+    rmq: RmqConfig
 }
