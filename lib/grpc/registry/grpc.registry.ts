1cfb38d5af7e83e189ac79c883e6ab4f6ebc8a11
Egor312-33 <olegsergeon4@gmail.com>
2026-02-26 20:45:05 +0700

    create: video grpc

--- lib/grpc/registry/grpc.registry.ts
+++ lib/grpc/registry/grpc.registry.ts
@@ -21,4 +21,9 @@ export const GRPC_CLIENTS = {
     protoPath: PROTO_PATHS.MEDIA,
     env: "MEDIA_GRPC_URL",
   },
+  VIDEO_PACKAGE: {
+    package: "video.v1",
+    protoPath: PROTO_PATHS.VIDEO,
+    env: "VIDEO_GRPC_URL",
+  },
 } as const;
